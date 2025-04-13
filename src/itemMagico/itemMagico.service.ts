87b19aa3bf061db6706440e35dcbade3cb581eac
Pedro <jplussani79@gmail.com>
2025-04-12 23:20:16 -0300

    Validações de valores de força e defesa dos itens

--- src/itemMagico/itemMagico.service.ts
+++ src/itemMagico/itemMagico.service.ts
@@ -13,6 +13,34 @@ export class ItemMagicoService {
     private readonly personagemService: PersonagemService,
   ) {}
 
+  private validarItem(item: CreateItemMagicoDto, personagemId: string): void {
+    const { tipo, forca, defesa } = item;
+
+    if (forca === 0 && defesa === 0) {
+      throw new BadRequestException('Um item mágico não pode ter Força e Defesa iguais a zero.');
+    }
+
+    if (forca > 10 || defesa > 10) {
+      throw new BadRequestException('Os atributos Força e Defesa não podem ser maiores que 10.');
+    }
+
+    if (tipo === 'Arma' && defesa !== 0) {
+      throw new BadRequestException('Itens do tipo Arma devem ter Defesa igual a zero.');
+    }
+
+    if (tipo === 'Armadura' && forca !== 0) {
+      throw new BadRequestException('Itens do tipo Armadura devem ter Força igual a zero.');
+    }
+
+    if (tipo === 'Amuleto') {
+      const personagem = this.personagemService.findById(personagemId);
+      const amuletosExistentes = personagem.itens.filter((i) => i.tipo === 'Amuleto');
+      if (amuletosExistentes.length > 0) {
+        throw new BadRequestException('O personagem já possui um item mágico do tipo Amuleto.');
+      }
+    }
+  }
+
   create(itemData: CreateItemMagicoDto): ItemMagico {
     const { personagemId, ...item } = itemData;
 
@@ -25,15 +53,37 @@ export class ItemMagicoService {
       throw new NotFoundException(`Personagem com ID ${personagemId} não encontrado.`);
     }
 
-    if (!personagem.itens) {
-      personagem.itens = [];
-    }
+    this.validarItem(itemData, personagemId);
 
     const novoItem: ItemMagico = { id: uuidv4(), personagemId, ...item };
     this.itensMagicos.push(novoItem);
     personagem.itens.push(novoItem);
 
     return novoItem;
+  }
+
+  update(id: string, updateData: Partial<CreateItemMagicoDto>): ItemMagico {
+    const index = this.itensMagicos.findIndex((i) => i.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Item mágico com ID ${id} não encontrado.`);
+    }
+
+    const itemExistente = this.itensMagicos[index];
+    const updatedItem: ItemMagico = { ...itemExistente, ...updateData };
+
+    this.validarItem(updatedItem, itemExistente.personagemId);
+
+    this.itensMagicos[index] = updatedItem;
+
+    const personagem = this.personagemService.findById(itemExistente.personagemId);
+    if (personagem) {
+      const idx = personagem.itens.findIndex((i) => i.id === id);
+      if (idx !== -1) {
+        personagem.itens[idx] = updatedItem;
+      }
+    }
+
+    return updatedItem;
   }
 
   findAll(): ItemMagico[] {
@@ -48,29 +98,8 @@ export class ItemMagicoService {
     return item;
   }
 
-  update(id: string, updateData: Partial<CreateItemMagicoDto>): ItemMagico {
-    const index = this.itensMagicos.findIndex(i => i.id === id);
-    if (index === -1) {
-      throw new NotFoundException(`Item mágico com ID ${id} não encontrado.`);
-    }
-    const itemExistente = this.itensMagicos[index];
-    const updatedItem: ItemMagico = { ...itemExistente, ...updateData };
-    
-    this.itensMagicos[index] = updatedItem;
-
-    const personagem = this.personagemService.findById(itemExistente.personagemId);
-    if (personagem) {
-      const idx = personagem.itens.findIndex(i => i.id === id);
-      if (idx !== -1) {
-        personagem.itens[idx] = updatedItem;
-      }
-    }
-    
-    return updatedItem;
-  }
-
   remove(id: string): void {
-    const index = this.itensMagicos.findIndex(i => i.id === id);
+    const index = this.itensMagicos.findIndex((i) => i.id === id);
     if (index === -1) {
       throw new NotFoundException(`Item mágico com ID ${id} não encontrado.`);
     }
@@ -79,7 +108,7 @@ export class ItemMagicoService {
 
     const personagem = this.personagemService.findById(removedItem.personagemId);
     if (personagem) {
-      const idx = personagem.itens.findIndex(i => i.id === id);
+      const idx = personagem.itens.findIndex((i) => i.id === id);
       if (idx !== -1) {
         personagem.itens.splice(idx, 1);
       }
